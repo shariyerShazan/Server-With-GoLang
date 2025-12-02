632faee5fa02863d461432fdf5b9d1520ec90bb8
shariyerShazan <shariyershazan1@gmail.com>
2025-12-03 00:20:29 +0600

    hello print handlerFunc with go

--- main.go
+++ main.go
@@ -6,6 +6,25 @@ import(
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter , r *http.Request){
+	if r.URL.Path != "/hello" {
+		http.Error(w , "404 not found" , http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {	
+		http.Error(w , "Method not supported" , http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w , "Hello")
+}
+
 func main(){
-	
+    fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/" , fileServer)
+	http.HandleFunc("/hello" , helloHandler)
+
+	fmt.Printf("Starting server at port :3333\n")
+	if err := http.ListenAndServe(":3333" , nil); err != nil {
+		log.Fatal(err)
+	}
 }
