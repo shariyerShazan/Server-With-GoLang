b3bf485a2cb2068cdf6239540ce0a8edcfaa6b1f
shariyerShazan <shariyershazan1@gmail.com>
2025-12-03 00:37:00 +0600

    server run with go

--- main.go
+++ main.go
@@ -18,6 +18,18 @@ func helloHandler(w http.ResponseWriter , r *http.Request){
 	fmt.Fprintf(w , "Hello")
 }
 
+func shazanHandler(w http.ResponseWriter , r *http.Request){
+	if r.URL.Path != "/shazan" {
+		http.Error(w , "404 not found" , http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {	
+		http.Error(w , "Method not supported" , http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w , "Shariyer Shazan's Web runnning!!!")
+}
+
 func formHanlder(w http.ResponseWriter, r *http.Request){
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w , "parseForm() err: %v" , err)
@@ -37,6 +49,7 @@ func main(){
 	http.Handle("/" , fileServer)
 	http.HandleFunc("/hello" , helloHandler)
 	http.HandleFunc("/form" , formHanlder)
+	http.HandleFunc("/shazan" , shazanHandler)
 
 	fmt.Printf("Starting server at port :3333\n")
 	if err := http.ListenAndServe(":3333" , nil); err != nil {
