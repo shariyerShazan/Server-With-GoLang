8e958a7b8259744e71c3c51c4045ad9db7801406
shariyerShazan <shariyershazan1@gmail.com>
2025-12-03 00:29:28 +0600

    formHnadler

--- main.go
+++ main.go
@@ -18,10 +18,25 @@ func helloHandler(w http.ResponseWriter , r *http.Request){
 	fmt.Fprintf(w , "Hello")
 }
 
+func formHanlder(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w , "parseForm() err: %v" , err)
+		return
+	}
+	fmt.Fprintf(w , "Post request succesSfully")
+
+	name := r.FormValue("name")
+	adress := r.FormValue("adress")
+
+	fmt.Fprintf(w , "Name : %s\n", name)
+	fmt.Fprintf(w , "Adress : %s\n", adress)
+}
+
 func main(){
     fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/" , fileServer)
 	http.HandleFunc("/hello" , helloHandler)
+	http.HandleFunc("/form" , formHanlder)
 
 	fmt.Printf("Starting server at port :3333\n")
 	if err := http.ListenAndServe(":3333" , nil); err != nil {
